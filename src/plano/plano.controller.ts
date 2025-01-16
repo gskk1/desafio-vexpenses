008f76d957723e2c50307fae6e5ab82f68df6859
Gustavo Eto <gustavo.eto1@hotmail.com>
2025-01-15 22:34:03 -0300

    Implementado service e controller dos planos

--- src/plano/plano.controller.ts
+++ src/plano/plano.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { PlanoService } from './plano.service';
+import { Plano } from './plano.model';
+import { Produto } from 'src/produto/produto.model';
 
 @Controller('plano')
-export class PlanoController {}
+export class PlanoController {
+    constructor(private readonly planoService: PlanoService) {}
+
+    @Get('')
+    async getPlanos():Promise<Plano[]> {
+    return this.planoService.getPlanos();
+    }
+
+    @Get(':id')
+    async getPlanosById(@Param('id') id: number):Promise<Plano> {
+    return this.planoService.getPlanosById(id);
+    }
+
+    @Post()
+    async create(@Body() body: { name: string, listaProdutos: Produto[]}): Promise<Plano> {
+      return this.planoService.createPlano(body.name, body.listaProdutos);
+    }
+
+    @Delete(':id')
+    async deleteById(@Param('id') id: number): Promise<{ message: string }> {
+    await this.planoService.deletePlanosById(id);
+    return { message: 'User deleted successfully' };
+    }
+
+
+}
--- src/plano/plano.service.ts
+++ src/plano/plano.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Plano } from './plano.model';
+import { Produto } from 'src/produto/produto.model';
+import { ProdutosPlano } from 'src/produtos-plano/produtos-plano.model';
 
 @Injectable()
-export class PlanoService {}
+export class PlanoService {
+    constructor(@InjectModel (Plano) private planoModel: typeof Plano,
+                @InjectModel (ProdutosPlano) private ProdutosPlanoModel: typeof ProdutosPlano,
+                @InjectModel (Produto) private ProdutoModel: typeof Produto,) {}
+
+    async createPlano(name: string, listaProdutos: Produto[]): Promise<Plano> {
+        listaProdutos.forEach(produto => {
+
+            this.ProdutosPlanoModel.create({produto})
+        });
+
+        return this.planoModel.create({ name });
+      }
+
+    async getPlanos(): Promise<Plano[]> {
+        return this.planoModel.findAll();
+      }
+
+    async getPlanosById(id: number): Promise<Plano> {
+        return this.planoModel.findByPk(id);
+      }
+
+    async deletePlanosById(id: number): Promise<void> {
+        const res = await this.planoModel.destroy({ where: {id}});
+      }
+}
